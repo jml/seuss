17ebeb216966467abf5b6c2dc0023954c1f2f317
Jonathan Lange <jml@mumak.net>
2022-06-08 17:27:11 +0100

    Add monadic parsers
    I need these to implement combinators like "count" or basic sequencing.
    I hope I can figure out a way to implement those without them.

--- src/seuss.py
+++ src/seuss.py
@@ -1,11 +1,13 @@
 """An experiment in Python parsers."""
 
 import string
-from typing import Protocol, TypeVar
+from typing import Callable, Generic, Protocol, TypeVar
 
 import attrs
 
 
+A = TypeVar("A")
+B = TypeVar("B")
 T = TypeVar("T")
 
 class Parser(Protocol[T]):
@@ -43,3 +45,28 @@ class Digit:
         if text[0] in string.digits:
             return [(text[0], text[1:])]
         return []
+
+
+@attrs.define
+class Pure(Generic[T]):
+    """Inject a value into the parsed result."""
+
+    # TODO: Maybe "inject" might be clearer?
+    value: T
+
+    def parse(self, text: str) -> list[tuple[T, str]]:
+        return [(self.value, text)]
+
+
+@attrs.define
+class AndThen(Generic[A, B]):
+    """Run another parser that's constructed with the output of the previous one."""
+
+    previous: Parser[A]
+    callback: Callable[[A], Parser[B]]
+
+    def parse(self, text: str) -> list[tuple[B, str]]:
+        results = []
+        for (value, remaining) in self.previous.parse(text):
+            results.extend(self.callback(value).parse(remaining))
+        return results
--- tests/test_seuss.py
+++ tests/test_seuss.py
@@ -1,6 +1,6 @@
 """Tests for seuss."""
 
-from seuss import Digit, String
+from seuss import AndThen, Digit, Pure, String
 
 def test_string() -> None:
     assert String("fnord").parse("fnord hello") == [("fnord", " hello")]
@@ -10,3 +10,20 @@ def test_string() -> None:
 def test_digit() -> None:
     assert Digit().parse("1989") == [("1", "989")]
     assert Digit().parse("foo 1989") == []
+
+
+def test_pure() -> None:
+    p = Pure(42)
+    assert p.parse("foo") == [(42, "foo")]
+
+
+def test_pure_and_then() -> None:
+    p = Pure("42")
+    q = AndThen(p, String)
+    assert q.parse("420") == String("42").parse("420")
+
+
+def test_and_then_pure() -> None:
+    p = Digit()
+    q = AndThen(p, Pure)
+    assert q.parse("420") == p.parse("420")
