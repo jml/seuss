113a92fb69905ac9f5bd472daa4ce6bc95492f29
Jonathan Lange <jml@mumak.net>
2022-06-08 16:47:17 +0100

    Add a couple of basic parsers

--- src/seuss.py
+++ src/seuss.py
@@ -1,1 +1,45 @@
 """An experiment in Python parsers."""
+
+import string
+from typing import Protocol, TypeVar
+
+import attrs
+
+
+T = TypeVar("T")
+
+class Parser(Protocol[T]):
+    """
+    A parser for things
+    Is a function from strings
+    To lists of pairs
+    Of things and strings.
+    """
+
+    def parse(self, text: str) -> list[tuple[T, str]]:
+        ...
+
+
+@attrs.define
+class String:
+    """Parse out a constant string."""
+    match: str
+
+    def parse(self, text: str) -> list[tuple[str, str]]:
+        if text.startswith(self.match):
+            return [(self.match, text[len(self.match):])]
+        return []
+
+
+@attrs.define
+class Digit:
+    """Parse a single digit.
+
+    Return it as a character to give us more flexibility in how we use it.
+    """
+    def parse(self, text: str) -> list[tuple[str, str]]:
+        if not text:
+            return []
+        if text[0] in string.digits:
+            return [(text[0], text[1:])]
+        return []
--- tests/test_seuss.py
+++ tests/test_seuss.py
@@ -1,5 +1,12 @@
 """Tests for seuss."""
 
+from seuss import Digit, String
 
-def test_pass() -> None:
-    assert True
+def test_string() -> None:
+    assert String("fnord").parse("fnord hello") == [("fnord", " hello")]
+    assert String("fnord").parse("hello world") == []
+
+
+def test_digit() -> None:
+    assert Digit().parse("1989") == [("1", "989")]
+    assert Digit().parse("foo 1989") == []
