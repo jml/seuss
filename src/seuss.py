1bb94346834081bf4fe44b2dbdb7ae90ab98196c
Jonathan Lange <jml@mumak.net>
2022-06-09 19:32:02 +0100

    Use iterators rather than lists

--- src/seuss.py
+++ src/seuss.py
@@ -1,7 +1,7 @@
 """An experiment in Python parsers."""
 
 import string
-from typing import Callable, Generic, Protocol, TypeVar
+from typing import Callable, Generic, Iterator, Protocol, TypeVar
 
 import attrs
 
@@ -11,14 +11,26 @@ B = TypeVar("B")
 T = TypeVar("T")
 
 class Parser(Protocol[T]):
-    """
-    A parser for things
-    Is a function from strings
-    To lists of pairs
-    Of things and strings.
+    """A monadic parser.
+
+    > A parser for things
+    > Is a function from strings
+    > To lists of pairs
+    > Of things and strings.
+
+    Note that here, a parser goes from strings to _iterators_ of pairs of
+    things and strings. This is because lists are lazy in Haskell but not in
+    Python, and we only want to go into other parsing options if we really
+    have to.
     """
 
-    def parse(self, text: str) -> list[tuple[T, str]]:
+    def parse(self, text: str) -> Iterator[tuple[T, str]]:
+        """Parse T out of text, and return what's left of text.
+
+        If we cannot parse the text, return an empty iterator.
+        If there are multiple possible ways to parse the text,
+        return an iterator that yields multiple results.
+        """
         ...
 
 
@@ -27,10 +39,9 @@ class String:
     """Parse out a constant string."""
     match: str
 
-    def parse(self, text: str) -> list[tuple[str, str]]:
+    def parse(self, text: str) -> Iterator[tuple[str, str]]:
         if text.startswith(self.match):
-            return [(self.match, text[len(self.match):])]
-        return []
+            yield (self.match, text[len(self.match):])
 
 
 @attrs.define
@@ -39,12 +50,9 @@ class Digit:
 
     Return it as a character to give us more flexibility in how we use it.
     """
-    def parse(self, text: str) -> list[tuple[str, str]]:
-        if not text:
-            return []
-        if text[0] in string.digits:
-            return [(text[0], text[1:])]
-        return []
+    def parse(self, text: str) -> Iterator[tuple[str, str]]:
+        if text and text[0] in string.digits:
+            yield (text[0], text[1:])
 
 
 @attrs.define
@@ -52,11 +60,9 @@ class Map(Generic[A, B]):
     function: Callable[[A], B]
     parser: Parser[A]
 
-    def parse(self, text: str) -> list[tuple[B, str]]:
-        result = []
+    def parse(self, text: str) -> Iterator[tuple[B, str]]:
         for (value, remaining) in self.parser.parse(text):
-            result.append((self.function(value), remaining))
-        return result
+            yield (self.function(value), remaining)
 
 
 @attrs.define
@@ -66,8 +72,8 @@ class Pure(Generic[T]):
     # TODO: Maybe "inject" might be clearer?
     value: T
 
-    def parse(self, text: str) -> list[tuple[T, str]]:
-        return [(self.value, text)]
+    def parse(self, text: str) -> Iterator[tuple[T, str]]:
+        yield (self.value, text)
 
 
 @attrs.define
@@ -77,15 +83,12 @@ class AndThen(Generic[A, B]):
     previous: Parser[A]
     callback: Callable[[A], Parser[B]]
 
-    def parse(self, text: str) -> list[tuple[B, str]]:
-        results = []
+    def parse(self, text: str) -> Iterator[tuple[B, str]]:
         for (value, remaining) in self.previous.parse(text):
-            results.extend(self.callback(value).parse(remaining))
-        return results
+            yield from self.callback(value).parse(remaining)
 
 # TODO: Some way of handling applicative
 # TODO: Parse a YYYY-MM-DD date
 # TODO: sequence
 # TODO: replicate
-# TODO: Turn lists into Iterables / Iterators
 # TODO: yield expression syntax?
--- tests/test_seuss.py
+++ tests/test_seuss.py
@@ -3,32 +3,32 @@
 from seuss import AndThen, Digit, Map, Pure, String
 
 def test_string() -> None:
-    assert String("fnord").parse("fnord hello") == [("fnord", " hello")]
-    assert String("fnord").parse("hello world") == []
+    assert list(String("fnord").parse("fnord hello")) == [("fnord", " hello")]
+    assert list(String("fnord").parse("hello world")) == []
 
 
 def test_digit() -> None:
-    assert Digit().parse("1989") == [("1", "989")]
-    assert Digit().parse("foo 1989") == []
+    assert list(Digit().parse("1989")) == [("1", "989")]
+    assert list(Digit().parse("foo 1989")) == []
 
 
 def test_pure() -> None:
     p = Pure(42)
-    assert p.parse("foo") == [(42, "foo")]
+    assert list(p.parse("foo")) == [(42, "foo")]
 
 
 def test_pure_and_then() -> None:
     p = Pure("42")
     q = AndThen(p, String)
-    assert q.parse("420") == String("42").parse("420")
+    assert list(q.parse("420")) == list(String("42").parse("420"))
 
 
 def test_and_then_pure() -> None:
     p = Digit()
     q = AndThen(p, Pure)
-    assert q.parse("420") == p.parse("420")
+    assert list(q.parse("420")) == list(p.parse("420"))
 
 
 def test_map() -> None:
     p = Map(int, Digit())
-    assert p.parse("420") == [(4, "20")]
+    assert list(p.parse("420")) == [(4, "20")]
